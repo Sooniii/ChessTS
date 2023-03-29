1d22d2077e0824d2df8d1346609e67a6fdba82ef
edouard <edouard.rucquoy@edu.itescia.fr>
2023-03-29 09:51:43 +0200

    (wip) demo complete data verification

--- src/demo-express/server.ts
+++ src/demo-express/server.ts
@@ -1,13 +1,18 @@
 import express from 'express'
-import { routerOne } from './router'
-import { routerAdmin } from './router-admin'
+import { routerBlog, routerUser } from './routes'
 
 export function initServer() {
   const app = express()
   app.use(express.json())
 
-  app.use(routerOne)
-  app.use('/admin', routerAdmin)
+  /* Middleware, console.log each request */
+  app.use((req, res, next) => {
+    console.log(req.method, req.path, +new Date())
+    next()
+  })
+
+  app.use('/blog', routerBlog)
+  app.use('/user', routerUser)
 
   app.listen(8080, () => {
     console.log('Listening on http://localhost:8080');
---
+++ src/demo-express/types/dto/blog.interface.ts
@@ -0,0 +1,5 @@
+export interface IPostBlog {
+  title: string
+  description: string
+  image_url: string
+}
