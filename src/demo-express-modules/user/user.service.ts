c7b3313b16bc2bd9d2c682000aca2c9453268204
Edouard <edrcq@users.noreply.github.com>
2023-04-17 14:14:29 +0200

    add service example

---
+++ src/demo-express-modules/user/user.service.ts
@@ -0,0 +1,10 @@
+import { IUserRegisterBody } from './user.interface';
+
+
+export function login(email: string, password: string) {
+  // return a JWT token
+}
+
+export function register(userData: IUserRegisterBody) {
+  // .. do something to register the user
+}
