3af1feab7fcd85c0838d4c07b0e7dc14b194d5a2
PaulRoumy <72387727+PaulRoumy@users.noreply.github.com>
2023-04-21 09:13:11 +0200

    service party

--- src/chess_party/routes.ts
+++ src/chess_party/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
-import { getAllParty } from './controller';
+import { addNewParty, getAllParty, getOneParty } from './controller';
 
 export const router = Router();
 
 router.get('/all', getAllParty);
+router.get('/find/:username', getOneParty);
+router.post('/add', addNewParty);
+
